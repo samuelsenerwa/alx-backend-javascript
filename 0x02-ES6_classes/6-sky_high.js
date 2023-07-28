03fe32b79a17fbca889ac5cfbfed18a445bf42d5
Samuel Senerwa <samuelsenerwa@gmail.com>
2023-07-28 02:28:28 -0700

    ES6 tasks

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,17 +1,16 @@
-/*eslint-disable*/
-import SkyHighBuilding from './6-sky_high.js';
+import Building from './5-building';
 
-export default class SkyHighBuilding {
-	constructor(sqft, floors) {
-		super(sqft);
-		this._floors = floors;
-	}
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-	get floors() {
-		return this._floors;
-	}
+  get floors() {
+    return this._floors;
+  }
 
-	evacuationWarningMessage() {
-		return `Evacuation slowly the ${this._floors} floors`;
-	}
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
