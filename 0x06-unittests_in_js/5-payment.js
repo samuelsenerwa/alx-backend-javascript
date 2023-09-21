bcbaa17910d4f62e88df7c6bad18671a22692c6c
samuelsenerwa <samuelsenerwa@gmail.com>
2023-09-21 15:21:40 +0300

    unittest

--- 0x06-unittests_in_js/5-payment.js
+++ 0x06-unittests_in_js/5-payment.js
@@ -1,4 +1,4 @@
-const utils = require("./utils");
+const Utils = require("./utils");
 
 function sendPaymentRequestToApi(totalAmount, totalShipping) {
   const result = Utils.calculateNumber("SUM", totalAmount, totalShipping);
