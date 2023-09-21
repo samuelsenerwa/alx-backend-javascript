20a21d93315280b2053d8b8c5e7f5d34c6f23bbb
samuelsenerwa <samuelsenerwa@gmail.com>
2023-09-21 16:15:45 +0300

    API unittest

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const { response } = require("../8-api/api");
+const { request } = require("chai");
+
+const app = express();
+const port = 7865;
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.send("Welcome to the payment system");
+});
+
+app.get("/cart/:id([0-9]+)", (request, response) => {
+  response.send(`Payment methods for the cart ${request.params.id}`);
+});
+
+app.get("/available_payments", (request, response) => {
+  response.set("Content-Type", "application/json");
+  const payMethods = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  };
+  response.send(payMethods);
+});
+
+app.post("/login", (request, response) => {
+  const userName = response.body.userName;
+  if (userName) {
+    response.send(`Welcome ${userName}`);
+  } else {
+    response.status(404).send();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
+
+module.exports = app;
